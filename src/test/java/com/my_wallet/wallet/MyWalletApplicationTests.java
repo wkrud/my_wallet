088a34857bde4cef8fc08d4178e7b288ae73ebb1
wkrud <jae6140@naver.com>
2022-04-17 13:03:24 +0900

    start project

---
+++ src/test/java/com/my_wallet/wallet/MyWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_wallet.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
